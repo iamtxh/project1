6a3c071cf13529a5f6e5a7cf84c38ae94bf59e8b
iamtxh <iamtxh@yahoo.com>
2017-06-06 16:05:04 +0800

    first UT case

---
+++ src/test/java/hello/FirstTest.java
@@ -0,0 +1,21 @@
+pakcage hello;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+
+@Tag("fast")
+class FirstTest {
+
+	@Test
+	@DisplayName("My 1st JUnit 5 test!")
+	void myFirstTest(TestInfo testInfo) {
+		Greeter greeter = new Greeter();
+		assertEquals("Hello world!", gretter.sayHello(), "ERROR!!!");
+		assertEquals("My 1st JUnit 5 test!", testInfo.getDisplayName(), () -> "TestInfo is injected correctly");
+	}
+
+}
